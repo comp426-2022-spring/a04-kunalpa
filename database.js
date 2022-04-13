c591944e099ccdb72ae33346de368d6c04b475d1
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-13 15:15:09 -0400

    edits

--- database.js
+++ database.js
@@ -22,7 +22,8 @@ if (row === undefined) {
     `; 
 // Execute SQL commands that we just wrote above. 
     db.exec(sqlInit); 
-} else { 
+}
+else { 
 // Since the database already exists, echo that to the console.
     console.log('Database exists.')
 } 
