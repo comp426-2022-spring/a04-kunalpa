3e7e7bd1a65a3944f3a9998f1910616ee4e4a522
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-13 15:16:58 -0400

    edits

--- database.js
+++ database.js
@@ -30,7 +30,7 @@ if (row === undefined) {
 else { 
     // Since the database already exists, echo that to the console.
     console.log('Database exists.')
-} 
+}
 
 // Export all of the above as a module so that we can use it elsewhere.
 module.exports = db
