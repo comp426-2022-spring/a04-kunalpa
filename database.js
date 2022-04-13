07231130f644462081c85b287cca2b4c26a12798
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-13 15:15:51 -0400

    edits

--- database.js
+++ database.js
@@ -10,6 +10,7 @@ const db = new Database('log.db');
 const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`);
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();  
+
 // Check if there is a table. If row is undefined then no table exists.
 if (row === undefined) { 
 // Echo information about what you are doing to the console.
@@ -20,7 +21,7 @@ if (row === undefined) {
     time INTEGER, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, 
     status INTEGER, referer TEXT, useragent TEXT );
     `; 
-    
+
 // Execute SQL commands that we just wrote above. 
     db.exec(sqlInit); 
 }
