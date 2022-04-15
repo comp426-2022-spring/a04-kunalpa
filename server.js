98d01775ad0a75416406bfa40714d60482819b75
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-15 14:08:50 -0400

    edits

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ server.js [options]
                 between 1 and 65535.  
   --debug	If set to true, creates endlpoints /app/log/access/ which returns
               	a JSON access log from the database and /app/error which throws 
-              	an error with the message "Error test successful." Defaults to 
+              	an error with the message "Error test successful." Defaults to  
 		false.
   --log		If set to false, no log files are written. Defaults to true.
 		Logs are always written to database.
